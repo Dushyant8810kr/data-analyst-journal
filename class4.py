6f3604db409d66e2f5886c7c9d8c1ef182063f9c
Dushyant kumar <dushyantkr8810@gmail.com>
2026-01-24 07:41:24 -0700

    class 4

---
+++ class4.py
@@ -0,0 +1,40 @@
+#Arithmetic operators(+,-,*,/,//,**,%) #they only work for  numbers datatype
+
+a=12
+b=34
+#print(a+b) -> addition #46
+#print(a-b) -> substraction #-22
+#print(a*b) -> multiplication #408
+#print(a/b) -> division #0.35294117647058826 (/ gives the answer in manner of Float)
+#print(a//b) -> division #0 (// they gives the ans. in integer term)
+#print(5 ** 2) -> power operator # 25 (5^2 = 25)
+#print(5%2) -> modulo operator # 1 (% they give the answer as remainder)
+
+#IMP:- we use BODMAS rule
+
+#Task 1 :-
+print((12+35)/35)
+
+
+#Assignment operators :-
+
+a = 12
+a = a+12 #(reassign   (ANS #24))
+
+a+=12 #(short hand operation)
+#examples :- += ,-= ,*=,/=,//=,%= 
+
+#strings also use some operatores like 
+# + for concatenation
+
+a ="hello"
+b= " brother"
+
+print(a+b) 
+
+#some extra things :-
+print("hello" + 3) # showing error because we concatente  strings only with strings
+
+print("hello"*3) # prints hello 3 times or as much as you can depends on you 
+
+

49f82b6715f65555f2e004b6a0fd6a529bbafb78
Dushyant kumar <dushyantkr8810@gmail.com>
2026-04-19 07:33:10 -0700

    class 23 : Oops intro

---
+++ python-basic/class23.py
@@ -0,0 +1,25 @@
+#Imperitive programming:-
+"""
+a =12 
+b=36
+print(a + b)
+"""
+
+#Functional programming :-
+"""
+def addition(a,b):
+    print(a+b)
+
+addition(12,36)
+addition(22,11)
+"""
+
+
+#Oops:-
+"""
+class addition:
+    def __init__(a,b):
+        print(a+b)
+
+obj = addition(12,12)
+"""

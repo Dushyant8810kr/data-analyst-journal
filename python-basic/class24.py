73f73d783aefcdf6adbba7831ca9dbb39804a490
Dushyant kumar <dushyantkr8810@gmail.com>
2026-04-21 07:42:04 -0700

    class 24 : class(update)

--- python-basic/class24.py
+++ python-basic/class24.py
@@ -1,12 +1,12 @@
 #class :- class is just like a blueprint or prototype
-class factory:
+class Factory:
     a = 12 #attribute :- a variable define in a class is known as attribute.
     def hello():
         print("how are you!!")  #method :- a funtion define in a class is known as method.
     
     print("I am getting initialized!!")
 
-print(factory.a)   #for accessing a varible that is inside a class
-factory.hello()    #for accessing a function that is inside a class
+print(Factory.a)   #for accessing a varible that is inside a class
+Factory.hello()    #for accessing a function that is inside a class
 
-#imp :- you class is initialized at very starting for onle once time.
+#imp :- your class is initialized at very starting for onle once time.
